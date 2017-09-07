9772023c843749163418e2da3c7fe79b9f0dfb58
Seth Ryals <RyalsSw1852@aguafria.org>
2017-09-07 10:26:40 -0700

    9/8

---
+++ js/quotes.js
@@ -0,0 +1,12 @@
+var actualQuote= document.getElementById("printQuote");
+
+var quotes = [
+  ""
+]
+
+var author = [
+  ""
+]
+
+var randomNumber = Math.floor(Math.random()* 9);
+actualQuote.innerHTML = author(random number);
